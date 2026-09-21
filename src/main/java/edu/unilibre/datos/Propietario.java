f4670e7e0b9e9ecd86ba5ac961ae167bb27804bb
Sahichel <shara.ahichel@gmail.com>
2026-09-20 19:42:14 -0500

    Encapsulamiento Propietario

--- src/main/java/edu/unilibre/datos/Propietario.java
+++ src/main/java/edu/unilibre/datos/Propietario.java
@@ -1,4 +1,13 @@
 package edu.unilibre.datos;
 
 public class Propietario {
+    private String identificacion;
+    private String nombre;
+
+    public String obtenerIdentificacion() {
+        return identificacion;
+    }
+    public String obtenerNombre() {
+        return nombre;
+    }
 }
