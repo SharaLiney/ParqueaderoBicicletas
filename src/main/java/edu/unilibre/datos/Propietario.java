0309bb9163aaf3f69c2de061c9251435bb76876a
Sahichel <shara.ahichel@gmail.com>
2026-09-20 22:05:22 -0500

    Más métodos

--- src/main/java/edu/unilibre/datos/Propietario.java
+++ src/main/java/edu/unilibre/datos/Propietario.java
@@ -10,4 +10,16 @@ public class Propietario {
     public String obtenerNombre() {
         return nombre;
     }
+
+    public void modificarIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+    public void modificarNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Propietario (String identificacion, String nombre){
+        this.identificacion=identificacion;
+        this.nombre = nombre;
+    }
 }
